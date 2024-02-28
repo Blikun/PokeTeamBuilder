cf0c6778979d124639573e5cd2d8a3d9ae0f5c22
keeo <pablorizosanchez@gmail.com>
2024-02-28 22:41:32 +0100

    api basis pokemon model

--- lib/screens/pokedex_screen.dart
+++ lib/screens/pokedex_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:poke_team_builder/api_client/fetch_pokemon.dart';
-import 'package:poke_team_builder/data/pokemon_index.dart';
+
 
 class PokedexScreen extends StatelessWidget {
   const PokedexScreen({super.key});
