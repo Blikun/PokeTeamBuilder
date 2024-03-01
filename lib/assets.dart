e51ab333cbaee461b93c3c0f01d2ce0654b31c55
keeo <pablorizosanchez@gmail.com>
2024-03-01 23:13:27 +0100

    Assets

---
+++ lib/assets.dart
@@ -0,0 +1,49 @@
+
+class Assets{
+
+  static const String pokeballAnim = 'assets/lotties/pokeball_loading_lottie.json';
+
+  static const String physical = 'assets/icons/physical.webp';
+  static const String special = 'assets/icons/special.webp';
+  static const String status = 'assets/icons/status.webp';
+
+  static const String substitute = 'assets/icons/substitute.webp';
+
+  static const String fireBg = 'assets/bgs/fire.webp';
+  static const String bugBg = 'assets/bgs/bug.webp';
+  static const String darkBg = 'assets/bgs/dark.webp';
+  static const String dragonBg = 'assets/bgs/dragon.webp';
+  static const String electricBg = 'assets/bgs/electric.webp';
+  static const String fairyBg = 'assets/bgs/fairy.webp';
+  static const String fightBg = 'assets/bgs/fighting.webp';
+  static const String flyingBg = 'assets/bgs/flying.webp';
+  static const String ghostBg = 'assets/bgs/ghost.webp';
+  static const String grassBg = 'assets/bgs/grass.webp';
+  static const String groundBg = 'assets/bgs/ground.webp';
+  static const String iceBg = 'assets/bgs/ice.webp';
+  static const String normalBg = 'assets/bgs/normal.webp';
+  static const String poisonBg = 'assets/bgs/poison.webp';
+  static const String psychicBg = 'assets/bgs/psychic.webp';
+  static const String rockBg = 'assets/bgs/rock.webp';
+  static const String steelBg = 'assets/bgs/steel.webp';
+  static const String waterBg = 'assets/bgs/water.webp';
+
+  static const String iconFire = 'assets/icons/fire.webp';
+  static const String iconBug = 'assets/icons/bug.webp';
+  static const String iconDark = 'assets/icons/dark.webp';
+  static const String iconDragon = 'assets/icons/dragon.webp';
+  static const String iconElectric = 'assets/icons/electric.webp';
+  static const String iconFairy = 'assets/icons/fairy.webp';
+  static const String iconFight = 'assets/icons/fighting.webp';
+  static const String iconFlying = 'assets/icons/flying.webp';
+  static const String iconGhost = 'assets/icons/ghost.webp';
+  static const String iconGrass = 'assets/icons/grass.webp';
+  static const String iconGround = 'assets/icons/ground.webp';
+  static const String iconIce = 'assets/icons/ice.webp';
+  static const String iconNormal = 'assets/icons/normal.webp';
+  static const String iconPoison = 'assets/icons/poison.webp';
+  static const String iconPsychic = 'assets/icons/psychic.webp';
+  static const String iconRock = 'assets/icons/rock.webp';
+  static const String iconSteel = 'assets/icons/steel.webp';
+  static const String iconWater = 'assets/icons/water.webp';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/get_instance.dart';
